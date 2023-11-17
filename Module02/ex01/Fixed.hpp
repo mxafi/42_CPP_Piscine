1c1f098826a2dab007879be2279ac12ddcab6247
Markus Laaksonen <markus@mxa.fi>
2023-11-17 09:41:20 +0200

    started Module02/ex01

---
+++ Module02/ex01/Fixed.hpp
@@ -0,0 +1,22 @@
+#include <cmath>
+#include <iostream>
+
+class Fixed {
+	private:
+		int					fixedPointNumberValue_;
+		static const int	numberOfFractionalBits_ = 8;
+	public:
+		// default constructor that initializes the fixed-point number value to 0
+		Fixed(void);
+		// copy constructor
+		Fixed(const Fixed& original);
+		// copy assignment operator overload. (could just call the copy constructor)
+		Fixed& operator = (const Fixed& original);
+		// destructor
+		~Fixed(void);
+		// returns the raw value of the fixed-point value
+		int		getRawBits(void) const;
+		// sets the raw value of the fixed-point number
+		void	setRawBits(int const raw);
+};
+
