ccac04ab8487b58ce49d5a30d86d612eb4becdf8
Markus Laaksonen <markus@mxa.fi>
2023-11-17 17:36:30 +0200

    reformat Module02/ex01 and add prototypes

--- Module02/ex01/Fixed.cpp
+++ Module02/ex01/Fixed.cpp
@@ -1,33 +1,40 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(void) {
+Fixed::Fixed(void)
+{
 	std::cout << "Default constructor called" << std::endl;
 	this->fixedPointNumberValue_ = 0;
 }
 
-Fixed::Fixed(const Fixed& original) {
+Fixed::Fixed(const Fixed &original)
+{
 	std::cout << "Copy constructor called" << std::endl;
 	this->fixedPointNumberValue_ = original.fixedPointNumberValue_;
 }
 
-Fixed& Fixed::operator = (const Fixed& original) {
+Fixed &Fixed::operator=(const Fixed &original)
+{
 	std::cout << "Copy assignment operator called" << std::endl;
-	if (this != &original) {
+	if (this != &original)
+	{
 		this->fixedPointNumberValue_ = original.fixedPointNumberValue_;
 	}
 	return *this;
 }
 
-Fixed::~Fixed(void) {
+Fixed::~Fixed(void)
+{
 	std::cout << "Destructor called" << std::endl;
 }
 
-int Fixed::getRawBits(void) const {
+int Fixed::getRawBits(void) const
+{
 	std::cout << "getRawBits member function called" << std::endl;
 	return this->fixedPointNumberValue_;
 }
 
-void Fixed::setRawBits(int const raw) {
+void Fixed::setRawBits(int const raw)
+{
 	std::cout << "setRawBits member function called" << std::endl;
 	this->fixedPointNumberValue_ = raw;
 }
