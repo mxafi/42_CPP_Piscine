ae626a2852b8609f4e9cc89a8f2b55552b581717
Markus Laaksonen <markus@mxa.fi>
2023-11-17 11:46:25 +0200

    prototype Module02/ex01

--- Module02/ex00/Fixed.hpp
+++ Module02/ex00/Fixed.hpp
@@ -9,13 +9,22 @@ class Fixed {
 		Fixed(void);
 		// copy constructor
 		Fixed(const Fixed& original);
-		// copy assignment operator overload. (could just call the copy constructor)
+		// copy assignment operator overload (could just call the copy constructor)
 		Fixed& operator = (const Fixed& original);
 		// destructor
 		~Fixed(void);
 		// returns the raw value of the fixed-point value
-		int		getRawBits(void) const;
+		int getRawBits(void) const;
 		// sets the raw value of the fixed-point number
-		void	setRawBits(int const raw);
+		void setRawBits(int const raw);
+		// const int constructor, fract bits 8 and converts to fixed point
+		Fixed(const int intN);
+		// const float constructor, frac bits 8 and converts to fixed point
+		Fixed(const float fpN);
+		// converts the fixed-point value to a floating-point value
+		float toFloat(void) const;
+		// converts the fixed-point value to an integer value
+		int toInt(void) const;
 };
-
+// insertion operator overload for Fixed class numbers to insert a float
+std::ostream& operator << (std::ostream& out, const Fixed& fixedNumber);
