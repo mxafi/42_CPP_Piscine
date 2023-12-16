7f5c033bcced5ea59f97200eade32b6448e8fdba
Markus Laaksonen <markus@mxa.fi>
2023-12-16 16:31:24 +0200

    add debug main

--- Module04/ex03/main.cpp
+++ Module04/ex03/main.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "AMateria.hpp"
 #include "Character.hpp"
 #include "Cure.hpp"
@@ -5,66 +6,134 @@
 #include "IMateriaSource.hpp"
 #include "Ice.hpp"
 #include "MateriaSource.hpp"
+#include "color.hpp"
 
 int main() {
-  IMateriaSource* src = new MateriaSource();
-  src->learnMateria(new Ice());
-  src->learnMateria(new Cure());
+  {
+    std::cout << COLOR_CYAN << "--- example test ---" << COLOR_RESET
+              << std::endl;
 
-  ICharacter* me = new Character("me");
-  AMateria* tmp;
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
 
-  tmp = src->createMateria("ice");
-  me->equip(tmp);
-  delete tmp;
-  tmp = src->createMateria("cure");
-  me->equip(tmp);
-  delete tmp;
-  ICharacter* bob = new Character("bob");
-  me->use(0, *bob);
-  me->use(1, *bob);
+    ICharacter* me = new Character("me");
 
-  Character originalCharacter("Original");
-  tmp = src->createMateria("ice");
-  originalCharacter.equip(tmp);
-  delete tmp;
-  Character copyCharacter(originalCharacter);
-  originalCharacter.use(0, *bob);
-  copyCharacter.use(0, *bob);
+    AMateria* tmp;
+    tmp = src->createMateria("ice");
+    me->equip(tmp);
+    tmp = src->createMateria("cure");
+    me->equip(tmp);
 
-  tmp = src->createMateria("ice");
-  me->equip(tmp);
-  delete tmp;
-  tmp = src->createMateria("cure");
-  me->equip(tmp);
-  delete tmp;
-  tmp = src->createMateria("ice");
-  me->equip(tmp);
+    ICharacter* bob = new Character("bob");
 
-  me->use(0, *bob);
-  me->use(1, *bob);
-  me->use(2, *bob);
-  me->use(3, *bob);
-  me->use(4, *bob);
+    me->use(0, *bob);
+    me->use(1, *bob);
 
-  me->unequip(0);
-  me->use(0, *bob);
-  me->unequip(1);
-  me->use(1, *bob);
-  me->unequip(2);
-  me->use(2, *bob);
-  me->unequip(3);
-  me->use(3, *bob);
-  me->unequip(4);
+    delete bob;
+    delete me;
+    delete src;
+  }
+  {
+    std::cout << COLOR_CYAN << "--- copy test ---" << COLOR_RESET << std::endl;
 
-  src->learnMateria(new Ice());
-  src->learnMateria(new Ice());
-  src->learnMateria(new Ice());
-  src->learnMateria(new Ice());
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+    IMateriaSource* src_copy =
+        new MateriaSource(dynamic_cast<MateriaSource&>(*src));
 
-  delete bob;
-  delete me;
-  delete src;
+    ICharacter* orig = new Character("orig");
+    orig->equip(src_copy->createMateria("ice"));
+    orig->equip(src_copy->createMateria("cure"));
+    orig->use(0, *orig);
+    orig->use(1, *orig);
 
+    ICharacter* copy = new Character(dynamic_cast<Character&>(*orig));
+    copy->use(0, *copy);
+    copy->use(1, *copy);
+
+    delete src;
+    delete src_copy;
+    delete orig;
+    delete copy;
+  }
+  {
+    std::cout << COLOR_CYAN << "--- overfill Character inventory test ---"
+              << COLOR_RESET << std::endl;
+
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+
+    ICharacter* me = new Character("me");
+
+    me->equip(src->createMateria("ice"));
+    me->equip(src->createMateria("ice"));
+    me->equip(src->createMateria("ice"));
+    me->equip(src->createMateria("ice"));
+    me->equip(src->createMateria("cure"));
+
+    me->use(0, *me);
+    me->use(1, *me);
+    me->use(2, *me);
+    me->use(3, *me);
+
+    me->unequip(2);
+    me->equip(src->createMateria("cure"));
+
+    me->use(0, *me);
+    me->use(1, *me);
+    me->use(2, *me);
+    me->use(3, *me);
+
+    delete src;
+    delete me;
+  }
+  {
+    std::cout << COLOR_CYAN << "--- overfill MateriaSource inventory test ---"
+              << COLOR_RESET << std::endl;
+
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Ice());
+    src->learnMateria(new Ice());
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+
+    delete src;
+  }
+  {
+    std::cout << COLOR_CYAN << "--- overfill Character pile test ---"
+              << COLOR_RESET << std::endl;
+
+    IMateriaSource* src = new MateriaSource();
+    src->learnMateria(new Ice());
+    src->learnMateria(new Cure());
+
+    ICharacter* me = new Character("me");
+
+    // fill the pile, inventory empty
+    for (int i = 0; i < 100; i++) {
+      me->equip(src->createMateria("ice"));
+      me->unequip(0);
+    }
+
+    // fill the inventory as well
+    for (int i = 0; i < 4; i++) {
+      me->equip(src->createMateria("ice"));
+    }
+
+    // unequip slot 1 and equip cure
+    // should not unequip because pile full, should not equip because inventory full
+    me->unequip(1);
+    me->equip(src->createMateria("cure"));
+
+    // check that slot 1 is still ice, not cure
+    me->use(1, *me);
+
+    delete src;
+    delete me;
+  }
   return 0;
 }
