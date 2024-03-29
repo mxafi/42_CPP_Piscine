6b9e3dea6eeb96e6c0103f5dbfa96197b595e04f
Markus Laaksonen <markus@mxa.fi>
2024-03-29 16:34:15 +0200

    add debug stuff and basics

--- Module06/ex00/ScalarConverter.hpp
+++ Module06/ex00/ScalarConverter.hpp
@@ -1,8 +1,27 @@
 #ifndef SCALARCONVERTER_HPP
 #define SCALARCONVERTER_HPP
 
-#include <string>
+#define RESET "\033[0m"
+#define CYAN "\033[36m"
+
+#ifdef DEBUG
+#define LOG_DEBUG(x) \
+  { std::cout << CYAN << "Debug: " << x << RESET << std::endl; };
+#define DEBUG_INPUT_ENUM_LABEL(x)                                     \
+  {                                                                   \
+    const char* inputEnumLabels[] = {"CHAR",    "INT",     "FLOAT",   \
+                                     "DOUBLE",  "INVALID", "IND_NAN", \
+                                     "POS_INF", "NEG_INF"};           \
+    LOG_DEBUG("ScalarConverter::ScalarConverter: inputType: "         \
+              << inputEnumLabels[x]);                                 \
+  };
+#else
+#define LOG_DEBUG(x)
+#define DEBUG_INPUT_ENUM_LABEL(x)
+#endif
+
 #include <iostream>
+#include <string>
 
 class ScalarConverter {
  private:
@@ -24,6 +43,11 @@ class ScalarConverter {
 
   ScalarType getScalarType(const std::string& input);
   void printOutput();
+  void parseChar(const std::string& input);
+  void parseInt(const std::string& input);
+  void parseFloat(const std::string& input);
+  void parseDouble(const std::string& input);
+  bool isDisplayable(char c);
 
   ScalarType inputType;
   std::string outputStringChar;
