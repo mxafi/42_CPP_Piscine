e7f7bbcadcc5d218c02f5f3754b1839557c5e4e7
Markus Laaksonen <markus@mxa.fi>
2024-03-29 18:22:09 +0200

    wip

--- Module06/ex00/ScalarConverter.cpp
+++ Module06/ex00/ScalarConverter.cpp
@@ -17,22 +17,23 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
 
 ScalarConverter::ScalarConverter(const std::string& input) {
   inputType = getScalarType(input);
+  DEBUG_INPUT_ENUM_LABEL(inputType);
   switch (inputType) {
     case CHAR:
-      // input[1] is the character
+      parseChar(input);
       break;
     case INT:
-      // stoi(input) is the integer
+      parseInt(input);
       break;
     case FLOAT:
-      // stof(input) is the float
+      parseFloat(input);
       break;
     case DOUBLE:
-      // stod(input) is the double
+      parseDouble(input);
       break;
     case INVALID:
-      // invalid input
-      break;
+      std::cout << "Invalid input" << std::endl;
+      return;
     case IND_NAN:
       // nan
       break;
@@ -56,6 +57,7 @@ void ScalarConverter::convert(const std::string& input) {
 // private utility methods
 ScalarConverter::ScalarType ScalarConverter::getScalarType(
     const std::string& input) {
+  LOG_DEBUG("ScalarConverter::getScalarType: got input: " << input);
   if (input.length() == 0) {
     return INVALID;
   } else if (input.length() == 3 && input[0] == '\'' && input[2] == '\'') {
@@ -66,6 +68,13 @@ ScalarConverter::ScalarType ScalarConverter::getScalarType(
     return NEG_INF;
   } else if (input == "nan" || input == "nanf") {
     return IND_NAN;
+  } else if (input.find_first_not_of("0123456789-.f") == std::string::npos) {
+    LOG_DEBUG("ScalarConverter::getScalarType: input is possibly int, float, or double");
+    if (input.find_first_of(".") == std::string::npos) {
+      LOG_DEBUG("ScalarConverter::getScalarType: input is possibly int");
+      // check if it fits in int, sanitize -.f positions
+      return INT;
+    }
   }
   return INVALID;
 }
@@ -75,4 +84,42 @@ void ScalarConverter::printOutput() {
   std::cout << "int: " << outputStringInt << std::endl;
   std::cout << "float: " << outputStringFloat << std::endl;
   std::cout << "double: " << outputStringDouble << std::endl;
+}
+
+void ScalarConverter::parseChar(const std::string& input) {
+  outputStringChar = input[1];
+  outputStringInt = std::to_string(static_cast<int>(input[1]));
+  outputStringFloat = std::to_string(static_cast<float>(input[1])) + "f";
+  outputStringDouble = std::to_string(static_cast<double>(input[1]));
+}
+
+void ScalarConverter::parseInt(const std::string& input) {
+  try {
+    int i = std::stoi(input);
+    outputStringInt = std::to_string(i);
+  } catch (std::exception& e) {
+    outputStringInt = "impossible";
+  }
+}
+
+void ScalarConverter::parseFloat(const std::string& input) {
+  try {
+    float f = std::stof(input);
+    outputStringFloat = std::to_string(f);
+  } catch (std::exception& e) {
+    outputStringFloat = "impossible";
+  }
+}
+
+void ScalarConverter::parseDouble(const std::string& input) {
+  try {
+    double d = std::stod(input);
+    outputStringDouble = std::to_string(d);
+  } catch (std::exception& e) {
+    outputStringDouble = "impossible";
+  }
+}
+
+bool isDisplayable(char c) {
+  return (c >= 32 && c <= 126);
 }
