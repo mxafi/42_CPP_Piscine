f40354896ce4c41660d0659c11190c422b88d966
Markus Laaksonen <markus@mxa.fi>
2024-04-15 15:52:32 +0300

    init Module07/ex00

---
+++ Module07/ex00/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <assert.h>
+
+#include "whatever.hpp"
+
+int main(void) {
+  std::cout << "==== START TESTS ============" << std::endl;
+
+  {
+    std::cout << "*=== example test ===========" << std::endl;
+
+    int a = 2;
+    int b = 3;
+
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+  }
+
+  std::cout << "*=== my tests w/o output ====" << std::endl;
+
+  // a is always greater than b and equal to c
+  // so min(a, b) should return b
+  // and max(a, b) should return a
+  // and min(a, c) should return c
+  // and max(a, c) should return c
+  int a = 42;
+  int b = 21;
+  int c = a;
+
+  assert(::min(a, b) == b);
+  assert(::max(a, b) == a);
+  assert(::min(a, c) == c);
+  assert(::max(a, c) == c);
+  swap(a, b);
+  assert(a == 21);
+  assert(b == 42);
+
+  std::cout << "==== END TESTS ==============" << std::endl;
+}
