915ad00a44d820c47040d928846b64ce853330d2
Markus Laaksonen <markus@mxa.fi>
2024-05-11 10:25:02 +0300

    style(M9e2): add notes

---
+++ Module09/ex02/main.cpp
@@ -0,0 +1,10 @@
+// Requirements:
+// Your program must be able to use a positive integer sequence as argument.
+// Your program must use the merge-insert sort algorithm to sort the positive integer sequence.
+// The algorithm must be the Ford-Johnson algorithm.
+
+// TODO: Have a PmergeMe class that can be instantiated
+// with a input vector or deque of integers.
+// The class should have a run method that will sort the input
+// the run method should return a sorted vector or deque of integers.
+// The main should time the runs for vector and deque.
