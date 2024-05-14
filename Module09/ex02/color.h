1a39f66f27320703fa1f2ab99ec40106e5ecc331
Markus Laaksonen <markus@mxa.fi>
2024-05-14 18:21:29 +0300

    feat(M9e2): add color.h

---
+++ Module09/ex02/color.h
@@ -0,0 +1,15 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define RESET "\033[0m"
+#define BLUE "\033[34m"
+#define YELLOW "\033[33m"
+#define GREEN "\033[32m"
+#define CYAN "\033[36m"
+#define RED "\033[31m"
+#define MAGENTA "\033[35m"
+#define LIGHT_YELLOW "\033[93m"
+#define LIGHT_GREEN "\033[92m"
+#define LIGHT_BLUE "\033[94m"
+
+#endif  // !COLOR_H
